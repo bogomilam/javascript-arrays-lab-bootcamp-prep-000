e2e91d0c601d24790b793dded50add41a39b88a3
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-27 22:37:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ function destructivelyAppendKitten(name){
 kittens.push(name)
 return kittens 
 }
+
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 return kittens
 }
+
 function destructivelyRemoveLastKitten(name){
 kittens.pop(name)
 return kittens
 }
+
+function 
